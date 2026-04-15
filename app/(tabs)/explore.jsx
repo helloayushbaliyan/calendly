60e82fe360142d0461fb0667c74691491bb492f2
Ayush Baliyan <helloayushbaliyan@gmail.com>
2026-04-15 15:53:36 +0530

    chore: update dependencies and add NativeWind support
    - Added babel-preset-expo and nativewind to package.json
    - Updated tsconfig.json to include nativewind-env.d.ts
    - Created TabLayout component for tab navigation
    - Added explore and index screens for tab navigation
    - Configured Babel with NativeWind support
    - Added global CSS for Tailwind styles
    - Configured Metro bundler for NativeWind
    - Created tailwind.config.js for Tailwind CSS setup
    - Added nativewind-env.d.ts for TypeScript support

---
+++ app/(tabs)/explore.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const explore = () => {
+  return (
+    <View>
+      <Text>explore</Text>
+    </View>
+  );
+};
+
+export default explore;
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti autem
+        asperiores cumque veritatis cum tempora odit corrupti beatae ex
+        consectetur quo magnam id quasi, harum, eligendi, explicabo minus.
+        Nostrum, laborum?
+      </Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
